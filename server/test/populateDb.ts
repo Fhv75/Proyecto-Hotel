fe8bf6a731ce4497e18977ca41429349e488e842
Fabián Andrés Henry Vilaxa <113631396+Fhv75@users.noreply.github.com>
2023-09-09 01:59:23 -0300

    Update populateDb.ts

--- server/test/populateDb.ts
+++ server/test/populateDb.ts
@@ -16,6 +16,7 @@ async function main() {
           pass: faker.internet.password(),
           nombre: faker.person.firstName(),
           telefono: faker.phone.number(),
+          rol: "user"
         },
       });
     }
